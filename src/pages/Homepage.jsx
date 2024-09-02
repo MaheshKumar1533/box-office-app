7d397134084e6204a17ced5275e0c90c8c7cf9a5
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2024-09-02 20:12:08 +0530

    Added api search

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const Homepage = () => {
-	return <>
-    HomePage
-    </>;
+	const [inputValue, setInputValue] = useState("");
+
+	const onInputChange = (event) => {
+		setInputValue(event.target.value);
+	};
+
+    const onSearch = async (event) => {
+        event.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`);
+        const body = await response.json();
+        console.log(body);
+    };
+
+	return (
+		<div>
+			<form onSubmit={onSearch}>
+				<input
+					type="text"
+					name="q"
+					value={inputValue}
+					onChange={onInputChange}
+				/>
+				<button
+					type="submit"
+				>
+					Search
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Homepage;
