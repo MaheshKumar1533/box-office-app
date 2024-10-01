17663897a244083b51ebe704b9874b21ef64d813
Mahesh Kumar Vaileti <maheshkumarvmk0@gmail.com>
2024-10-01 18:47:09 +0530

    Added Episodes and Cast in Shows

--- src/components/shows/Cast.jsx
+++ src/components/shows/Cast.jsx
@@ -1,5 +1,12 @@
 const Cast = ( { cast } ) => {
-    return <div>{ cast }</div>;
+    return <div>
+        {cast.map(( person ) => (
+            <div key={person.id}>
+                <img src={person.person.image.medium} alt="" />
+                <p>{person.person.name} | {person.character.name}</p>
+            </div>
+        ))}
+    </div>;
 }
 
 export default Cast;
--- src/components/shows/Seasons.jsx
+++ src/components/shows/Seasons.jsx
@@ -1,5 +1,17 @@
 const Seasons = ( { seasons } ) => {
-    return <div>{seasons}</div>
+    return (
+		<div>
+            <h2>Seasons</h2>
+            <p>Seasons in Total: {seasons.length}</p>
+            <p>Episodes in Total: {seasons.reduce((sum, season) => (sum + season.episodeOrder), 0)}</p>
+			<div>{seasons.map((e) => (
+                <div key={e.id}>
+                    <p>Season: {e.number}</p>
+                    <p>{e.episodeOrder ? `Episodes: ${e.episodeOrder}` : ''}</p>
+                </div>
+            ))}</div>
+		</div>
+	);
 }
 
 export default Seasons;
